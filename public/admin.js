25e3ed5d750a3a42e489594925788c257011a383
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2022-08-18 22:47:14 -0400

    ADMIN-PORTAL ASS

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,37 @@
 
 // Your Code Here
+
+let submitButton = document.createElement('button')
+submitButton.textContent = 'Save'
+
+async function main() {
+
+    let response = await fetch('http://localhost:3001/listBooks')
+
+    let books = await response.json()
+    console.log(books)
+}
+
+
+
+async function updateBook(book) {
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let submitButton = document.createElement('button')
+    submitButton.textContent = 'Save'
+
+    let response = await fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: book.id,
+            quantity: quantityInput.value
+        })
+    })
+    let updatedBook = await response.json();
+    console.log(updatedBook)
+}
